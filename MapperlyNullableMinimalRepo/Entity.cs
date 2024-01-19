653ed489c6a5528e818aa27fd391f5025ff2c33e
Paweł Pająk <pawel.pajak@hotmail.com>
2024-01-19 16:15:42 +0100

    init

---
+++ MapperlyNullableMinimalRepo/Entity.cs
@@ -0,0 +1,31 @@
+﻿using Riok.Mapperly.Abstractions;
+
+namespace MapperlyNullableMinimalRepo;
+
+public record InnerRecord(string SomeString);
+
+public class Entity
+{
+    private Entity(int id, InnerRecord innerRecord)
+    {
+        Id = id;
+        InnerRecord = innerRecord;
+    }
+
+    public int Id { get; }
+    public InnerRecord InnerRecord { get; }
+
+    public static Entity Create(int id, InnerRecord innerRecord)
+    {
+        return new Entity(id, innerRecord);
+    }
+}
+
+public record Dto(int Id, InnerRecord InnerRecord);
+
+[Mapper]
+public static partial class EntityMapper
+{
+    public static partial Dto ToDto(this Entity spool);
+    public static partial IQueryable<Dto> ProjectToDto(this IQueryable<Entity> form);
+}
---
+++ MapperlyNullableMinimalRepo/EntityMapperTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace MapperlyNullableMinimalRepo;
+
+public class EntityMapperTests
+{
+    [Test]
+    public void ToDto_ShouldMapEntityToDto()
+    {
+        // Arrange
+        var entity = Entity.Create(1, new InnerRecord("SomeValue"));
+
+        // Act
+        var dto = EntityMapper.ToDto(entity);
+
+        // Assert
+        Assert.That(dto, Is.Not.Null);
+        Assert.That(entity.Id, Is.EqualTo(1));
+        Assert.That(entity.InnerRecord.SomeString, Is.EqualTo("SomeValue"));
+    }
+}
